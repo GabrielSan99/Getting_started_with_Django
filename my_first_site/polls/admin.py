16bad39463190dce0a518632609502f9eeb33b8d
GabrielSan99 <gabriel_j.sanches@hotmail.com>
2022-03-27 14:12:19 -0300

    pages_creation

--- my_first_site/polls/admin.py
+++ my_first_site/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Question
+
+admin.site.register(Question) #Traz as informações do Question para a página admin
